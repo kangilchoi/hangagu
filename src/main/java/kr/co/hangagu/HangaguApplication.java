c3b4208ee5eae23520e8058cb0ee339f818bd592
SeHyeon Oh <osh5656@gmail.com>
2021-03-07 12:02:16 +0900

    프로젝트 commit

---
+++ src/main/java/kr/co/hangagu/HangaguApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hangagu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangaguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangaguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/hangagu/HangaguApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hangagu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangaguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
